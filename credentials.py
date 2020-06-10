6f6fc906d0edf068d4fc7a57d073b2afdc09319f
Nikhil Widhani <46464842+nikhil96widhani@users.noreply.github.com>
2020-06-10 12:50:08 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
 credentials = {
-    "MapBox_Token":"pk.eyJ1IjoibmlraGlsOTZ3aWRoYW5pIiwiYSI6ImNrM3p4aW5nMjBhdGMzZXMxdjhndWYyczMifQ.zOzJZkhUYpyZQRfb1XNlmQ",
+    "MapBox_Token":" {Your mapbox token without {} } ",
 }
